ed6c2a490cf92ddda6ede49b4bbb67cb051184c1
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 14:41:07 +0530

    with base condition

--- Recursion/src/sample_with_base_condition.java
+++ Recursion/src/sample_with_base_condition.java
@@ -1,8 +1,8 @@
 public class sample_with_base_condition {
-    static int c = 0;
+    static int c = 1;
     static void recurse() {
+        if (c == 4) return;
         System.out.println(c++);
-        if (c == 10) return;
         recurse();
     }
 
