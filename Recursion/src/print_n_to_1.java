939ac5c48cd5f4e8f87677febe198d537d506308
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 15:46:55 +0530

    with base condition

---
+++ Recursion/src/print_n_to_1.java
@@ -0,0 +1,11 @@
+public class print_n_to_1 {
+    static void printN(int n){
+        if(n==0) return;
+        System.out.println(n);
+        printN(n=n-1);
+    }
+
+    public static void main(String[] args) {
+        printN(5);
+    }
+}
