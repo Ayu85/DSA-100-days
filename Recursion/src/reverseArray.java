680fb30c1188915c3dd7f0df314bf6196011a782
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-23 18:22:11 +0530

    using backtracking

--- Recursion/src/reverseArray.java
+++ Recursion/src/reverseArray.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class reverseArray {
     static void rev(int[] arr, int start, int end) {
-        if (start > end) return;
+        if (start > end/2) return;
         int temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
@@ -10,7 +10,7 @@ public class reverseArray {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4};
         rev(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
