30d526569044d2a957187fc693cdb0f5780a26b2
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-23 19:08:01 +0530

    fibonacci

--- Recursion/src/fibonacci.java
+++ Recursion/src/fibonacci.java
@@ -3,11 +3,14 @@ public class fibonacci {
         int first = 0;
         int second = 1;
         System.out.print(first + " " + second + " ");
-        for (int i = 0; i < n-2; i++) {
+        for (int i = 0; i <= n - 3; i++) {
             int sum = first + second;
             first = second;
             second = sum;
-            System.out.print(sum + " ");
+           System.out.print(sum + " ");
+
+            if (i== n -3)
+                System.out.println("nth :"+ sum);
         }
     }
 
