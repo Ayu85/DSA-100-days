7e125bb5f163a58ab8c4a80a33400f0fce40e20a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-23 14:04:22 +0530

    using backtracking

--- Recursion/src/sum_1_to_n.java
+++ Recursion/src/sum_1_to_n.java
@@ -1,10 +1,10 @@
 public class sum_1_to_n {
-    static int sum( int n, int totalSum) {
-        if (n>5) return totalSum;
-        return sum(n + 1, totalSum + n);
+    static int sum(int i, int n, int totalSum) {
+        if (i > n) return totalSum;
+        return sum(i + 1, n, totalSum + i);
     }
 
     public static void main(String[] args) {
-        System.out.println(sum( 1, 0));
+        System.out.println(sum(1, 5, 0));
     }
 }
