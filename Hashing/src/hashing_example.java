a00e3c8111759090e7a1589b1241d874f8f2d08c
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-23 20:09:12 +0530

    hashing is too easy

--- Hashing/src/hashing_example.java
+++ Hashing/src/hashing_example.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class hashing_example {
     static int getOccur(int[] arr, int n, int target) {
-        int[] temp = new int[n];
+        int[] temp = new int[n+1];
         for (int x : arr)
             temp[x]++;
         System.out.println(Arrays.toString(temp));
@@ -11,7 +11,7 @@ public class hashing_example {
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 1, 2, 3, 1, 3, 5};
-        System.out.println(getOccur(arr,10,2));
+        int[] arr = {2, 1, 2, 3, 1, 3, 5,0,1,10};
+        System.out.println(getOccur(arr,10,10));
     }
 }
