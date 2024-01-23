c5f2df90e928882b2aafa213984e877308379735
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-23 18:32:24 +0530

    using backtracking

---
+++ Recursion/src/palindrome_string.java
@@ -0,0 +1,13 @@
+public class palindrome_string {   //civic
+    static boolean checkString(String str, int start, int end) {
+        if (start > end || str.toLowerCase().charAt(start) != str.toLowerCase().charAt(end)) return false;
+        checkString(str, start + 1, end - 1);
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "large";
+        System.out.println(checkString(str, 0, str.length() - 1));
+    }
+}
