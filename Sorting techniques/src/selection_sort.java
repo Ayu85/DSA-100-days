71b34785cb92fe116a7557df3dd1b215eb888670
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-24 14:27:38 +0530

    selection sort

--- Sorting techniques/src/selection_sort.java
+++ Sorting techniques/src/selection_sort.java
@@ -1,2 +1,23 @@
+import java.util.Arrays;
+
 public class selection_sort {
+    static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min])
+                    min = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr={8,2,3,-2,-9,0,1,4,6};
+        System.out.println("Array before sorting: "+ Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println("Array after sorting: "+ Arrays.toString(arr));
+    }
 }
