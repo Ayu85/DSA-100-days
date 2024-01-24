cc53f6db799a672ae21078f5121d052eea9622a7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-24 12:45:33 +0530

    char hashing optimal

--- Hashing/src/character_hashing.java
+++ Hashing/src/character_hashing.java
@@ -3,13 +3,19 @@ public class character_hashing {
         int c = 0;
         for (char x : arr)
             if (x == target) c++;
-
-
         return c;
+    }
+
+    static int getCharCount(char[] arr, char target) {
+        int[] hash = new int[26];
+        for (char x : arr)
+            if (x == target) hash[x - 97]++;
+
+        return hash[target-97];
     }
 
     public static void main(String[] args) {
         char[] arr = {'r', 'z', 'a', 'r', 'a'};
-        System.out.println(getCount(arr, 'a'));
+        System.out.println(getCharCount(arr, 'x'));
     }
 }
