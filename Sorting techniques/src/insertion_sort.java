56f19008acd71dccc97f26b4b0b4bf10ec701738
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-24 17:03:03 +0530

    insertion sort

--- Sorting techniques/src/insertion_sort.java
+++ Sorting techniques/src/insertion_sort.java
@@ -3,20 +3,19 @@ import java.util.Arrays;
 public class insertion_sort {
     static void insertionSort(int[] arr) {
         for (int j = 0; j < arr.length - 1; j++) {
-            for (int i = 1; i < arr.length - 1; i++) {
-                if (arr[i] < arr[i - 1]) {
-                    while (arr[i] < arr[i - 1] && i >= 0) {
-                        int temp = arr[i];
-                        arr[i] = arr[i - 1];
-                        arr[i - 1] = temp;
-                    }
+            for (int i = 1; i < arr.length ; i++) {
+                while (arr[i] < arr[i - 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i - 1];
+                    arr[i - 1] = temp;
                 }
             }
         }
     }
 
+
     public static void main(String[] args) {
-        int[] arr = {14, 9, 15, 12, 6, 8, 13};
+        int[] arr = {5,8,1,-8,0,-1};
         System.out.println("Array before sorting: " + Arrays.toString(arr));
         insertionSort(arr);
         System.out.println("Array after sorting: " + Arrays.toString(arr));
