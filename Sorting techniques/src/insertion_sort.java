2ed7cae1f1423cb5d785eec91a57a5ff9366f3e7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-24 17:06:03 +0530

    insertion sort optimal case

--- Sorting techniques/src/insertion_sort.java
+++ Sorting techniques/src/insertion_sort.java
@@ -3,22 +3,27 @@ import java.util.Arrays;
 public class insertion_sort {
     static void insertionSort(int[] arr) {
         for (int j = 0; j < arr.length - 1; j++) {
+            boolean isMoves=false;
             for (int i = 1; i < arr.length ; i++) {
                 while (arr[i] < arr[i - 1]) {
                     int temp = arr[i];
                     arr[i] = arr[i - 1];
                     arr[i - 1] = temp;
+                    isMoves=true;
                 }
             }
+            if(isMoves==false) return;
+            System.out.println("ran");
         }
     }
 
 
     public static void main(String[] args) {
         int[] arr = {5,8,1,-8,0,-1};
-        System.out.println("Array before sorting: " + Arrays.toString(arr));
-        insertionSort(arr);
-        System.out.println("Array after sorting: " + Arrays.toString(arr));
+        int[] arr2={1,2,3,4,5};
+        System.out.println("Array before sorting: " + Arrays.toString(arr2));
+        insertionSort(arr2);
+        System.out.println("Array after sorting: " + Arrays.toString(arr2));
 
     }
 }
