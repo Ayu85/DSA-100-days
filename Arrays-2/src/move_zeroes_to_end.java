34ff38d2df05422eaffc4823b681d7f3046f9290
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-27 20:43:09 +0530

    move zeroes to end

---
+++ Arrays-2/src/move_zeroes_to_end.java
@@ -0,0 +1,22 @@
+public class move_zeroes_to_end {
+    static void moveZeroesToEnd(int[] arr, int n) {
+        int j = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != 0) {
+                j++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        for (int x : arr)
+            System.out.print(x + " ");
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+        moveZeroesToEnd(arr, arr.length);
+
+    }
+}
