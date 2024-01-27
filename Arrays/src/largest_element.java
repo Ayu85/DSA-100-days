4e0038b320327e1e6cd63768d6092429d4172f05
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-27 13:14:12 +0530

    largest element in the array

--- Arrays/src/largest_element.java
+++ Arrays/src/largest_element.java
@@ -1,2 +1,15 @@
 public class largest_element {
+    static int getLargest(int[] arr) {
+        int max = arr[0];
+        for (int x : arr) {
+            if (x > max)
+                max = x;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={2,-1,0,4,8,3};
+        System.out.println(getLargest(arr));
+    }
 }
