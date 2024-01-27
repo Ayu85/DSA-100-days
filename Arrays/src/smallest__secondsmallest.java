8090e32c071629d3157cd2710a5cdfec1149af69
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-27 14:02:36 +0530

    second smallest element in the array

--- Arrays/src/smallest__secondsmallest.java
+++ Arrays/src/smallest__secondsmallest.java
@@ -8,8 +8,21 @@ public class smallest__secondsmallest {
         return small;
     }
 
+    static int secSmallest(int[] arr) {
+        int small = arr[0];
+        int secSmall = small;
+        for (int x : arr) {
+            if (x < small) {
+                secSmall = small;
+                small = x;
+            } else if (x < secSmall)
+                secSmall = x;
+        }
+        return secSmall;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {2, 1, 0, -2, 1, 50};
-        System.out.println(getSmallest(arr));
+        int[] arr = {2, 1, -2, 1, 50};
+        System.out.println(secSmallest(arr));
     }
 }
