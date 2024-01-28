382c8e73c291c91660ba71a9c172472ac1ca8565
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-28 13:46:36 +0530

    union of two sorted

---
+++ Arrays-2/src/union_of_two_sorted.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class union_of_two_sorted {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {1, 4, 5, 6};
+        getUnion(arr1, arr2);
+    }
+
+    static void getUnion(int[] arr1, int[] arr2) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] <= arr2[j]) {
+                if (temp.size() == 0 || temp.get(temp.size() - 1) != arr1[i]) temp.add(arr1[i]);
+                i++;
+            } else {
+                if (temp.size() == 0 || temp.get(temp.size() - 1) != arr2[j]) temp.add(arr2[j]);
+                j++;
+            }
+
+        }
+        for (int x : temp)
+            System.out.print(x + " ");
+    }
+}
