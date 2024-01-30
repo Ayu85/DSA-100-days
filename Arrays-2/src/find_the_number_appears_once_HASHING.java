f6e007b3ae0672519698763b363969b1ee86c2a8
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-30 18:47:27 +0530

    number appearing once using hashing

---
+++ Arrays-2/src/find_the_number_appears_once_HASHING.java
@@ -0,0 +1,16 @@
+public class find_the_number_appears_once_HASHING {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 3, 4, 4};
+        System.out.println(getNum(arr,arr.length));
+    }
+
+    static int getNum(int[] arr, int n) {
+        int[] temp = new int[arr[n - 1] + 1];
+        for (int x : arr)
+            temp[x]++;
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i] == 1) return i;
+        }
+        return -1;
+    }
+}
