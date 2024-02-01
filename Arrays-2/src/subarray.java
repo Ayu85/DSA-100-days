83cca902588b1fa91ad05dc0707278b46929f2ac
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-01 11:52:34 +0530

    subarray

---
+++ Arrays-2/src/subarray.java
@@ -0,0 +1,10 @@
+public class subarray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        printSubarray(arr,arr.length);
+    }
+
+    static int printSubarray(int[] arr, int n) {
+    for(int i=0)
+    }
+}
