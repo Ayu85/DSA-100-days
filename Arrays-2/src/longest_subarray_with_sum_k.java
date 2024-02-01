c7071393cf6fc7bf27354c065d3d456d07650ca2
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-01 12:03:47 +0530

    subarray sum better

--- Arrays-2/src/longest_subarray_with_sum_k.java
+++ Arrays-2/src/longest_subarray_with_sum_k.java
@@ -1,19 +1,17 @@
 public class longest_subarray_with_sum_k {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 1, 1, 1, 1, 4, 2, 3};
-        System.out.println(getLongestSubarray(arr,arr.length,3));
+        System.out.println(getLongestSubarray(arr, arr.length, 3));
     }
 
     static int getLongestSubarray(int[] arr, int n, int k) {
         int maxLength = -1;
         for (int i = 0; i < n; i++) {
+            int sum = 0;
+            int length = 0;
             for (int j = i; j < n; j++) {
-                int sum = 0;
-                int length = 0;
-                for (int m = i; m <= j; m++) {
-                    sum += arr[m];
-                    length++;
-                }
+                sum += arr[j];
+                length++;
                 if (sum == k && length > maxLength)
                     maxLength = length;
             }
