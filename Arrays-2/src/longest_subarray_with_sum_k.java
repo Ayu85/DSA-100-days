8301174381265920538d70e2e38c207dfe4727de
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-01 11:55:34 +0530

    subarray

---
+++ Arrays-2/src/longest_subarray_with_sum_k.java
@@ -0,0 +1,5 @@
+public class longest_subarray_with_sum_k {
+    public static void main(String[] args) {
+
+    }
+}
--- Arrays-2/src/subarray.java
+++ Arrays-2/src/subarray.java
@@ -1,10 +1,17 @@
 public class subarray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        printSubarray(arr,arr.length);
+        printSubarray(arr, arr.length);
     }
 
-    static int printSubarray(int[] arr, int n) {
-    for(int i=0)
+    static void printSubarray(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+        }
     }
 }
