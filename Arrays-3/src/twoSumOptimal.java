d157972d58a46f928acb6ccfe83a57acdf9ca974
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-03 20:46:32 +0530

    two sum optimal done

--- Arrays-3/src/twoSumOptimal.java
+++ Arrays-3/src/twoSumOptimal.java
@@ -1,20 +1,32 @@
+import java.util.Arrays;
+
 public class twoSumOptimal {
     public static void main(String[] args) {
         int[] arr = {2, 5, 6, 8, 11};
         System.out.println(isTwoSum(arr, 7));
+        System.out.println(Arrays.toString(getTwoSum(arr, 7)));
+
     }
 
     static boolean isTwoSum(int[] arr, int target) {
         int left = 0;
         int right = arr.length - 1;
         while (left <= right) {
-            if (arr[left] + arr[right] == target)
-                return true;
-            else if (arr[left] + arr[right] > target)
-                right--;
-            else
-                left++;
+            if (arr[left] + arr[right] == target) return true;
+            else if (arr[left] + arr[right] > target) right--;
+            else left++;
         }
         return false;
+    }
+
+    static int[] getTwoSum(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            if (arr[left] + arr[right] == target) return new int[]{left, right};
+            else if (arr[left] + arr[right] > target) right--;
+            else left++;
+        }
+        return new int[]{-1, -1};
     }
 }
