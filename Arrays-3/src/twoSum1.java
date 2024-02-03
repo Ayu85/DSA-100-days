e93fbbf676e33e2cbae6f4d2b8e9a3792c843d75
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-03 19:37:58 +0530

    two sum variation 1 done

--- Arrays-3/src/twoSum1.java
+++ Arrays-3/src/twoSum1.java
@@ -1,6 +1,16 @@
 public class twoSum1 {
     public static void main(String[] args) {
-
+        int[] arr = {2, 6, 5, 8, 11};
+        System.out.println(isTwoSum(arr, 15));
     }
 
+    static String isTwoSum(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target)
+                    return "yes";
+            }
+        }
+        return "no";
+    }
 }
