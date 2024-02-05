dec35564fec8dd68f6338dbd17ea220dde28e1cc
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-05 13:19:48 +0530

    majority element optimal

--- Arrays-3/src/majority_element_moores_voting_algo.java
+++ Arrays-3/src/majority_element_moores_voting_algo.java
@@ -1,6 +1,6 @@
 public class majority_element_moores_voting_algo {
     public static void main(String[] args) {
-        int[] nums = {7,7,5,7,5,1,5,7,5,5,7,7,5,5,5,5};
+        int[] nums = {7, 7, 5, 7, 5, 1, 5, 7, 5, 5, 7, 7, 5, 5, 5, 5};
         System.out.println(getMajElm(nums, nums.length));
     }
 
@@ -9,11 +9,12 @@ public class majority_element_moores_voting_algo {
         int count = 0;
         for (int i = 0; i < n; i++) {
             if (arr[i] == elm) count++;
-            else count--;
-            if (count == 0) {
+            else if (count == 0) {
                 elm = arr[i + 1];
                 count = 0;
             }
+            else
+                count--;
         }
         return elm;
     }
