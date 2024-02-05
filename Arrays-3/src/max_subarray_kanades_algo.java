70c153bb10a2342d21488a2b47f6648ae583c494
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-05 13:50:59 +0530

    max subarray sum optimal

--- Arrays-3/src/max_subarray_kanades_algo.java
+++ Arrays-3/src/max_subarray_kanades_algo.java
@@ -1,12 +1,14 @@
 public class max_subarray_kanades_algo {
     public static void main(String[] args) {
-        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
-        System.out.println(getMaxSum(arr, arr.length));
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] arr2 = {2};
+        System.out.println(getMaxSum(arr2, arr2.length));
     }
 
     static int getMaxSum(int[] nums, int n) {
         int sum = 0;
         int maxSum = Integer.MIN_VALUE;
+        if (n == 1) return nums[0];
         for (int x : nums) {
             sum += x;
             if (sum > maxSum)
@@ -14,6 +16,6 @@ public class max_subarray_kanades_algo {
             if (sum < 0)
                 sum = 0;
         }
-        return  maxSum;
+        return maxSum;
     }
 }
