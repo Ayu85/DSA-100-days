798092835296e9b054415d3e7c7eb189b25dc67b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-06 19:16:26 +0530

    rearrange by sign brute

--- Arrays-3/src/rearrange_elements_by_sign.java
+++ Arrays-3/src/rearrange_elements_by_sign.java
@@ -1,6 +1,26 @@
+import java.util.Arrays;
+
 public class rearrange_elements_by_sign {
     public static void main(String[] args) {
-
+        int[] arr = {2, -1, 3, -5, -4, 4};
+        System.out.println(Arrays.toString(arrangeElements(arr, arr.length)));
     }
 
+    static int[] arrangeElements(int[] nums, int n) {
+        int[] temp = new int[n];
+        int o = 0;
+        int p = 1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                temp[o] = nums[i];
+                o += 2;
+            }
+            else{
+                temp[p] = nums[i];
+                p += 2;
+            }
+
+        }
+        return temp;
+    }
 }
