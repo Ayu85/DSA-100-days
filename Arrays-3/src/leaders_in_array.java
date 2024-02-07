d715f81b74ea5a5419db73c54cdf0d7cfbeb2482
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-07 17:18:48 +0530

    leaders in array

--- Arrays-3/src/leaders_in_array.java
+++ Arrays-3/src/leaders_in_array.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class leaders_in_array {
     public static void main(String[] args) {
-        int[] arr = {10, 22, 12, 3, 0, 6};
+        int[] arr = {10, 22, 12, 3, 0, 5};
         ArrayList<Integer> res = getLeaders(arr, arr.length);
         for (int x : res)
             System.out.print(x + " ");
@@ -10,7 +10,7 @@ public class leaders_in_array {
 
     static ArrayList<Integer> getLeaders(int[] nums, int n) {
         ArrayList<Integer> temp = new ArrayList<>();
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n; i++) {
             boolean isLeader = true;
             for (int j = i + 1; j < n; j++) {
                 if (nums[j] > nums[i]) {
@@ -20,7 +20,7 @@ public class leaders_in_array {
             }
             if (isLeader) temp.add(nums[i]);
         }
-        temp.add(nums[n - 1]);
+//        temp.add(nums[n - 1]);
         return temp;
     }
 }
