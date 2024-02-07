cc3056593ce5a04e3da77bec3b4ab0a436575eae
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-07 17:13:21 +0530

    leaders in array

---
+++ Arrays-3/src/leaders_in_array.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class leaders_in_array {
+    public static void main(String[] args) {
+        int[] arr = {10, 22, 12, 3, 0, 6};
+        ArrayList<Integer> res = getLeaders(arr, arr.length);
+        for (int x : res)
+            System.out.print(x + " ");
+    }
+
+    static ArrayList<Integer> getLeaders(int[] nums, int n) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < n - 1; i++) {
+            boolean isLeader = true;
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] > nums[i]) {
+                    isLeader = false;
+                    break;
+                }
+            }
+            if (isLeader) temp.add(nums[i]);
+        }
+        temp.add(nums[n - 1]);
+        return temp;
+    }
+}
