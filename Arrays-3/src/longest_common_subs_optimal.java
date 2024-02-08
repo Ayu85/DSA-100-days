526e453a1242fe408d606f126043f54878b14869
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 19:47:04 +0530

    better

---
+++ Arrays-3/src/longest_common_subs_optimal.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class longest_common_subs_optimal {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 3, 2, 1, 4};
+        System.out.println(getLongestSub(arr, arr.length));
+    }
+
+    static int getLongestSub(int[] arr, int n) {
+        Arrays.sort(arr);
+        int max = 1;
+        int count = 0;
+        int lastSmall = Integer.MIN_VALUE;
+        for (int x : arr) {
+            if (lastSmall == x - 1) {
+                count++;
+                lastSmall = x;
+            } else if (lastSmall != x) {
+                count = 1;
+                lastSmall = x;
+            }
+            max = count > max ? count : max;
+
+
+        }
+        return max;
+    }
+}
