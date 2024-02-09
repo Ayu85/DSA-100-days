5621a988530025053cae9bb58f37bdec5bf8365c
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 16:21:49 +0530

    brute

--- Arrays-3/src/set_matrix_zeroes.java
+++ Arrays-3/src/set_matrix_zeroes.java
@@ -1,27 +1,47 @@
 public class set_matrix_zeroes {
     public static void main(String[] args) {
+        int[][] arr = {{1, 1, 1, 1}, {1, 0, 0, 1}, {1, 1, 0, 1}, {1, 1, 1, 1}};
+        System.out.println("Before marking zeroes");
+        printMatrix(arr, 4, 4);
+        System.out.println("After marking zeroes");
+        setMatrixZeroes(arr, 4, 4);
+        printMatrix(arr, 4, 4);
 
     }
 
-    static void markRow(int[] arr, int row) {
-        for (int i = 0; i < row; i++)
-            if (arr[i] != 0)
-                arr[i] = -1;
+    static void markRow(int[][] arr, int row) {
+        for (int i = 0; i < arr.length; i++)
+            if (arr[row][i] != 0)
+                arr[row][i] = -1;
     }
 
-    static void markCol(int[] arr, int col) {
-        for (int i = 0; i < col; i++)
-            if (arr[i] != 0)
-                arr[i] = -1;
+    static void markCol(int arr[][], int col) {
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i][col] != 0)
+                arr[i][col] = -1;
     }
 
-    static void setMatrixZeroes(int[] arr, int m, int n) {
+    static void printMatrix(int[][] arr, int m, int n) {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (arr[m][n] == 0) {
+                System.out.print(arr[i][j] + "    ");
+            }
+            System.out.println();
+        }
+    }
 
+    static void setMatrixZeroes(int[][] arr, int m, int n) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] == 0) {
+                    int row = i;
+                    int col = j;
+                    markRow(arr, row);
+                    markCol(arr, col);
                 }
             }
         }
     }
+
+
 }
