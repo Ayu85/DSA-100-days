11118f68aedcdc60a057dd1e0fbf02123e3af903
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-10 18:26:51 +0530

    brute force

--- Arrays-3/src/rotate_matrix_90deg.java
+++ Arrays-3/src/rotate_matrix_90deg.java
@@ -1,9 +1,9 @@
 public class rotate_matrix_90deg {
     public static void main(String[] args) {
-        int[][] a = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        int[][] res = ratate(a, 4, 4);
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+        int[][] a = {{1, 2, 3}, {5, 6, 7}, {9, 10, 11}};
+        int[][] res = ratate(a, 3, 3);
+        for (int i = 0; i <3; i++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(res[i][j] + "    ");
             }
             System.out.println();
@@ -12,7 +12,7 @@ public class rotate_matrix_90deg {
 
     static int[][] ratate(int[][] mat, int m, int n) {
         int[][] temp = new int[m][n];
-        int y = 3;
+        int y = m-1;
         for (int i = 0; i < m; i++) {
             int x = 0;
             for (int j = 0; j < n; j++) {
