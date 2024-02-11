29c5aac33227458055e80ef94ad34bf22224d50a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-11 23:26:29 +0530

    optimal

--- Arrays-3/src/spiral_matrix.java
+++ Arrays-3/src/spiral_matrix.java
@@ -2,7 +2,7 @@ public class spiral_matrix {
     public static void main(String[] args) {
         int[][] arr = {{1, 2, 3, 4, 5}, {16, 17, 18, 19, 6}, {15, 24, 25, 20, 7}, {14, 23, 22, 14, 8}, {13, 12, 11, 10, 9}};
         int[][] arr2 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-        printSpiral(arr2, arr2.length, arr2[0].length);
+        printSpiral(arr, arr.length, arr[0].length);
     }
 
     static void printSpiral(int[][] mat, int m, int n) {
