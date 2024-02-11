5ba78ba9ed528b2c117cae530b444c5ba986c811
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-11 18:41:27 +0530

    fucking solving the problems easily

--- Arrays-3/src/spiral_matrix.java
+++ Arrays-3/src/spiral_matrix.java
@@ -1,7 +1,7 @@
 public class spiral_matrix {
     public static void main(String[] args) {
-        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        printSpiral(arr, 3, 3);
+        int[][] arr = {{1, 2, 3,4,5}, {16,17,18,19,6}, {15,24,25,20,7},{14,23,22,14,8},{13,12,11,10,9}};
+        printSpiral(arr, 5, 5);
     }
 
     static void printSpiral(int[][] mat, int m, int n) {
@@ -9,20 +9,25 @@ public class spiral_matrix {
         int left = 0;
         int bottom = n - 1;
         int right = m - 1;
-        for (int i = left; i <= right; i++) {
-            System.out.print(mat[top][i] + " ");
+
+        while(left<=right&&top<=bottom){
+            for (int i = left; i <= right; i++) {
+                System.out.print(mat[top][i] + " ");
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(mat[i][right] + " ");
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                System.out.print(mat[bottom][i] + " ");
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                System.out.print(mat[i][left] + " ");
+            }
+            left++;
         }
-        top++;
-        for (int i = top; i <= bottom; i++) {
-            System.out.print(mat[i][right] + " ");
-        }
-        right--;
-        for (int i = right; i >= left; i--) {
-            System.out.print(mat[bottom][i] + " ");
-        }
-        bottom--;
-        for (int i = bottom; i >= top; i--) {
-            System.out.print(mat[i][left] + " ");
-        }
+
     }
 }
