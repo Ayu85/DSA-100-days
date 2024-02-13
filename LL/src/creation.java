0ec8ae1d321d2d68b5ddb40d602011d6e86bb9fe
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-13 16:23:13 +0530

    ll access

--- LL/src/creation.java
+++ LL/src/creation.java
@@ -1,18 +1,21 @@
 public class creation {
     public static void main(String[] args) {
         int[] arr = {2, 5, 1, 4, 8};
-        Node head= new Node(arr[0]);
-        Node temp=new Node(arr[4]);
-        head.next=temp;
-        System.out.println(head.value);
+        Node head = new Node(arr[0]);
+        Node mover=head;
+        for (int i = 1; i < arr.length-1; i++) {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover=temp;
+        }
 
 
     }
 }
 
 class Node {
-     int value;
-     Node next;
+    int value;
+    Node next;
 
     public Node(int value) {
         this.value = value;
