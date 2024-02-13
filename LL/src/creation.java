cfed02c0bfeb9754ecc6741f6cc243f218883a34
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-13 16:11:44 +0530

    ll creation

--- LL/src/creation.java
+++ LL/src/creation.java
@@ -1,8 +1,10 @@
 public class creation {
     public static void main(String[] args) {
-        Node n1=new Node(2);
+        int[] arr = {2, 5, 1, 4, 8};
+        Node head= new Node(arr[0]);
     }
 }
+
 class Node {
     private int value;
     private Node next;
@@ -10,6 +12,7 @@ class Node {
     public Node(int value) {
         this.value = value;
     }
+
     public Node(int value, Node next) {
         this.value = value;
         this.next = next;
