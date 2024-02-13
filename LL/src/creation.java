32d70223d309b5e3d6dca3cc877c28887e765804
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-13 16:26:05 +0530

    array conversion to linked list

--- LL/src/creation.java
+++ LL/src/creation.java
@@ -1,14 +1,19 @@
 public class creation {
-    public static void main(String[] args) {
-        int[] arr = {2, 5, 1, 4, 8};
+    static Node convert(int[] arr) {
         Node head = new Node(arr[0]);
-        Node mover=head;
-        for (int i = 1; i < arr.length-1; i++) {
+        Node mover = head;
+        for (int i = 1; i < arr.length; i++) {
             Node temp = new Node(arr[i]);
             mover.next = temp;
-            mover=temp;
+            mover = temp;
         }
+        return head;
+    }
 
+    public static void main(String[] args) {
+        int[] arr = {28, 5, 1, 4, 8};
+        Node head=convert(arr);
+        System.out.println(head.value);
 
     }
 }
