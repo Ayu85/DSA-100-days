0e0d6182ac469a10ee5c0a617f7409560c691a5f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-14 11:50:25 +0530

    reverse a string

--- Strings/src/palindrome.java
+++ Strings/src/palindrome.java
@@ -1,14 +1,24 @@
 public class palindrome {
     public static void main(String[] args) {
-        System.out.println(checkPal("abcb"));
+        System.out.println(checkPal("c@ivi@ c"));
 
     }
-    static boolean checkPal(String str){
-        int start=0;
-        int end =str.length()-1;
-        while(start<=end){
-            if(str.charAt(start++)!=str.charAt(end--))
+
+    static boolean checkPal(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+        while (start <= end) {
+            if (str.charAt(start) == ' ') {
+                start++;
+                break;
+            } else if (str.charAt(end) == ' ') {
+                end--;
+                break;
+
+            } else if (str.charAt(start++) != str.charAt(end--))
                 return false;
+
+
         }
         return true;
 
