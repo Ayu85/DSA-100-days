ab616e32ac5714b6416de38f8bfb515da6a8cddd
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-14 12:31:33 +0530

    check palindrome ignoring spaces and case bug fixes

--- Strings/src/validPalindrome.java
+++ Strings/src/validPalindrome.java
@@ -3,20 +3,26 @@ public class validPalindrome {
         int start = 0;
         int end = str.length() - 1;
         while (start <= end) {
-            if (str.charAt(start) < 65 || (str.charAt(start) > 90 && str.charAt(start) < 97))
+            if (str.charAt(start) < 65 || (str.charAt(start) > 90 && str.charAt(start) < 97)) {
                 start++;
-            else if (str.charAt(end) < 97 || (str.charAt(end) > 90 || str.charAt(end) > 122))
-                end--;
-            else {
-                if (str.toLowerCase().charAt(start++) != str.toLowerCase().charAt(end--))
-                    return false;
+                break;
             }
+            else if (str.charAt(end) > 122 || (str.charAt(end) > 90 && str.charAt(end) <97)) {
+                end--;
+                break;
+            }
+            else {
+                if (str.toLowerCase().charAt(start) != str.toLowerCase().charAt(end))
+                    return false;
+
+            }
+            start++;end--;
         }
         return true;
     }
 
     public static void main(String[] args) {
         System.out.println(checkValid("A man, a plan, a canal: Panama"));
-        System.out.println(checkValid("m@add*am"));
+        System.out.println(checkValid("race a car"));
     }
 }
