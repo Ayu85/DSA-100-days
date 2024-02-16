472b36d8dbc571183c9f7bcae78faf36dd0184b5
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-16 10:37:33 +0530

    try it by hashing

--- Strings/src/macOccuringChar.java
+++ Strings/src/macOccuringChar.java
@@ -1,33 +1,10 @@
 public class macOccuringChar {
     public static void main(String[] args) {
-        System.out.println(getMaxChar("abcdefabcdef"));
+        System.out.println(getMaxChar("fdsalkjhfh"));
     }
 
-    static char getMaxChar(String str) {
-        char max = str.charAt(0);
-        char maxChar = str.charAt(0);
-        int i = 0;
-        int count = 0;
-        int maxCount = Integer.MIN_VALUE;
-        char prev='a';
-        while (i < str.length()) {
-            for (int j = 0; j < str.length(); j++) {
-                if (str.charAt(j) == str.charAt(i)) {
-                    count++;
-                    max = str.charAt(j);
+    static char getMaxChar(String line) {
 
-                }
-            }
-            if (count > maxCount) {
-              maxChar=max;
-            }
-            else if(count==maxCount)
-            {
-                prev=max>maxChar?max:maxChar;
-            }
-            i++;
-        }
 
-        return prev;
     }
 }
