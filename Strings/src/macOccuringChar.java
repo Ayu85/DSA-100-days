a26ecaeb54af681c64f65477c60cd1469b9a3bd2
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-16 10:23:36 +0530

    brute force approach and fixed bugs

--- Strings/src/macOccuringChar.java
+++ Strings/src/macOccuringChar.java
@@ -1,27 +1,33 @@
 public class macOccuringChar {
     public static void main(String[] args) {
-        System.out.println(getMaxChar("output"));
+        System.out.println(getMaxChar("abcdefabcdef"));
     }
 
     static char getMaxChar(String str) {
-        char max = Character.MAX_LOW_SURROGATE;
-        char maxChar=str.charAt(0);
+        char max = str.charAt(0);
+        char maxChar = str.charAt(0);
         int i = 0;
         int count = 0;
-        int maxCount=Integer.MIN_VALUE;
+        int maxCount = Integer.MIN_VALUE;
+        char prev='a';
         while (i < str.length()) {
             for (int j = 0; j < str.length(); j++) {
                 if (str.charAt(j) == str.charAt(i)) {
                     count++;
-                    max=str.charAt(j);
+                    max = str.charAt(j);
+
                 }
             }
-            if(count>maxCount && max>maxChar)
-                maxChar=max;
-
+            if (count > maxCount) {
+              maxChar=max;
+            }
+            else if(count==maxCount)
+            {
+                prev=max>maxChar?max:maxChar;
+            }
             i++;
         }
 
-        return max;
+        return prev;
     }
 }
