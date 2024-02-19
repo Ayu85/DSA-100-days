81f5c72d53e06213e655912884229b5c23289bde
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-19 14:01:37 +0530

    creating linked lists

---
+++ LinkedList/src/Creation.java
@@ -0,0 +1,16 @@
+public class Creation {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        next = null;
+    }
+}
+
