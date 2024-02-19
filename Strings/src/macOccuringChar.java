658894d345b7104f9381efd70f77c3432813eb6f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-19 13:54:34 +0530

    starting linked lists

--- Strings/src/macOccuringChar.java
+++ Strings/src/macOccuringChar.java
@@ -1,10 +1,20 @@
+import java.util.Arrays;
+
 public class macOccuringChar {
     public static void main(String[] args) {
-        System.out.println(getMaxChar("fdsalkjhfh"));
+        System.out.println(Arrays.toString(getMaxChar("textsample")));
     }
 
-    static char getMaxChar(String line) {
+    static int[] getMaxChar(String line) {
+        int[] arr = new int[26];
+        for (int i = 0; i < line.length(); i++) {
+            int count = 0;
+            for (int j = 0; j < line.length(); j++) {
+                if (line.charAt(j) == line.charAt(i))
+                    
+            }
 
-
+        }
+        return arr;
     }
 }
