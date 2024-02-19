4d62d47c2011451613e24e881e5aa3d79bc381e2
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-19 14:05:27 +0530

    creating linked lists multiple nodes

--- LinkedList/src/Creation.java
+++ LinkedList/src/Creation.java
@@ -1,5 +1,11 @@
 public class Creation {
     public static void main(String[] args) {
+        Node head = new Node(10);
+        Node node1 = new Node(20);
+        Node node2 = new Node(30);
+        head.next = node1;
+        node1.next = node2;
+
 
     }
 }
